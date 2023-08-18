b34eb453a7ecd11f6b3f853f89a2119f0fb2ca09
Muhamad Muchlis <tru3.d3v@gmail.com>
2023-08-18 22:27:42 +0700

    add new

---
+++ src/main/java/io/debezium/connector/oracle/converters/TimestampConverter2.java
@@ -0,0 +1,173 @@
+package io.debezium.connector.oracle.converters;
+
+import io.debezium.spi.converter.CustomConverter;
+import io.debezium.spi.converter.RelationalColumn;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.kafka.connect.data.SchemaBuilder;
+
+public class TimestampConverter2 implements CustomConverter<SchemaBuilder, RelationalColumn> {
+
+    private static final Map<String, String> MONTH_MAP = Map.ofEntries(Map.entry("jan", "01"), Map.entry("feb", "02"),
+            Map.entry("mar", "03"), Map.entry("apr", "04"), Map.entry("may", "05"), Map.entry("jun", "06"),
+            Map.entry("jul", "07"), Map.entry("aug", "08"), Map.entry("sep", "09"), Map.entry("oct", "10"),
+            Map.entry("nov", "11"), Map.entry("dec", "12"));
+    public static final int MILLIS_LENGTH = 13;
+
+    public static final String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    public static final String DEFAULT_DATE_FORMAT = "YYYY-MM-dd";
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss.SSS";
+
+    public static final List<String> SUPPORTED_DATA_TYPES = List.of("date", "time", "datetime", "timestamp",
+            "datetime2");
+
+    private static final String DATETIME_REGEX = "(?<datetime>(?<date>(?:(?<year>\\d{4})-(?<month>\\d{1,2})-(?<day>\\d{1,2}))|(?:(?<day2>\\d{1,2})\\/(?<month2>\\d{1,2})\\/(?<year2>\\d{4}))|(?:(?<day3>\\d{1,2})-(?<month3>\\w{3})-(?<year3>\\d{4})))?(?:\\s?T?(?<time>(?<hour>\\d{1,2}):(?<minute>\\d{1,2}):(?<second>\\d{1,2})\\.?(?<milli>\\d{0,7})?)?))";
+    private static final Pattern regexPattern = Pattern.compile(DATETIME_REGEX);
+
+    public String strDatetimeFormat, strDateFormat, strTimeFormat;
+    public Boolean debug;
+
+//    private final SchemaBuilder datetimeSchema = SchemaBuilder.string().optional().name("oryanmoshe.time.DateTimeString");
+
+    private SimpleDateFormat simpleDatetimeFormatter, simpleDateFormatter, simpleTimeFormatter;
+
+    @Override
+    public void configure(Properties props) {
+        this.strDatetimeFormat = props.getProperty("format.datetime", DEFAULT_DATETIME_FORMAT);
+        this.simpleDatetimeFormatter = new SimpleDateFormat(this.strDatetimeFormat);
+
+        this.strDateFormat = props.getProperty("format.date", DEFAULT_DATE_FORMAT);
+        this.simpleDateFormatter = new SimpleDateFormat(this.strDateFormat);
+
+        this.strTimeFormat = props.getProperty("format.time", DEFAULT_TIME_FORMAT);
+        this.simpleTimeFormatter = new SimpleDateFormat(this.strTimeFormat);
+
+        this.debug = props.getProperty("debug", "false").equals("true");
+
+        this.simpleDatetimeFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        this.simpleTimeFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        if (this.debug)
+            System.out.printf(
+                    "[TimestampConverter.configure] Finished configuring formats. this.strDatetimeFormat: %s, this.strTimeFormat: %s%n",
+                    this.strDatetimeFormat, this.strTimeFormat);
+    }
+
+    @Override
+    public void converterFor(RelationalColumn column, ConverterRegistration<SchemaBuilder> registration) {
+        if (this.debug)
+            System.out.printf(
+                    "[TimestampConverter.converterFor] Starting to register column. column.name: %s, column.typeName: %s, column.hasDefaultValue: %s, column.defaultValue: %s, column.isOptional: %s%n",
+                    column.name(), column.typeName(), column.hasDefaultValue(), column.defaultValue(), column.isOptional());
+        if (SUPPORTED_DATA_TYPES.stream().anyMatch(s -> s.equalsIgnoreCase(column.typeName()))) {
+            boolean isTime = "time".equalsIgnoreCase(column.typeName());
+            // Use a new SchemaBuilder every time in order to avoid changing "Already set" options
+            // in the schema builder between tables.
+            registration.register(SchemaBuilder.string().optional(), rawValue -> {
+                if (rawValue == null) {
+                    // DEBUG
+                    if (this.debug) {
+                        System.out.printf("[TimestampConverter.converterFor] rawValue of %s is null.%n", column.name());
+                    }
+
+                    if (column.isOptional()) {
+                        return null;
+                    }
+                    else if (column.hasDefaultValue()) {
+                        return column.defaultValue();
+                    }
+                    return rawValue;
+                }
+
+                Long millis = getMillis(rawValue.toString(), isTime);
+                if (millis == null)
+                    return rawValue.toString();
+
+                Instant instant = Instant.ofEpochMilli(millis);
+                Date dateObject = Date.from(instant);
+                if (this.debug)
+                    System.out.printf(
+                            "[TimestampConverter.converterFor] Before returning conversion. column.name: %s, column.typeName: %s, millis: %d%n",
+                            column.name(), column.typeName(), millis);
+                switch (column.typeName().toLowerCase()) {
+                    case "time":
+                        return this.simpleTimeFormatter.format(dateObject);
+                    case "date":
+                        return this.simpleDateFormatter.format(dateObject);
+                    default:
+                        return this.simpleDatetimeFormatter.format(dateObject);
+                }
+            });
+        }
+    }
+
+    private Long getMillis(String timestamp, boolean isTime) {
+        if (timestamp.isBlank())
+            return null;
+
+        if (timestamp.contains(":") || timestamp.contains("-")) {
+            return milliFromDateString(timestamp);
+        }
+
+        int excessLength = timestamp.length() - MILLIS_LENGTH;
+        long longTimestamp = Long.parseLong(timestamp);
+
+        if (isTime)
+            return longTimestamp;
+
+        if (excessLength < 0)
+            return longTimestamp * 24 * 60 * 60 * 1000;
+
+        long millis = longTimestamp / (long) Math.pow(10, excessLength);
+        return millis;
+    }
+
+    private Long milliFromDateString(String timestamp) {
+        Matcher matches = regexPattern.matcher(timestamp);
+
+        if (matches.find()) {
+            String year = (matches.group("year") != null ? matches.group("year")
+                    : (matches.group("year2") != null ? matches.group("year2") : matches.group("year3")));
+            String month = (matches.group("month") != null ? matches.group("month")
+                    : (matches.group("month2") != null ? matches.group("month2") : matches.group("month3")));
+            String day = (matches.group("day") != null ? matches.group("day")
+                    : (matches.group("day2") != null ? matches.group("day2") : matches.group("day3")));
+            String hour = matches.group("hour") != null ? matches.group("hour") : "00";
+            String minute = matches.group("minute") != null ? matches.group("minute") : "00";
+            String second = matches.group("second") != null ? matches.group("second") : "00";
+            String milli = matches.group("milli") != null ? matches.group("milli") : "000";
+
+            if (milli.length() > 3)
+                milli = milli.substring(0, 3);
+
+            String dateStr = "";
+            dateStr += String.format("%s:%s:%s.%s", ("00".substring(hour.length()) + hour),
+                    ("00".substring(minute.length()) + minute), ("00".substring(second.length()) + second),
+                    (milli + "000".substring(milli.length())));
+
+            if (year != null) {
+                if (month.length() > 2)
+                    month = MONTH_MAP.get(month.toLowerCase());
+
+                dateStr = String.format("%s-%s-%sT%sZ", year, ("00".substring(month.length()) + month),
+                        ("00".substring(day.length()) + day), dateStr);
+            } else {
+                dateStr = String.format("%s-%s-%sT%sZ", "2020", "01", "01", dateStr);
+            }
+
+            Date dateObj = Date.from(Instant.parse(dateStr));
+            return dateObj.getTime();
+        }
+
+        return null;
+    }
+}
---
+++ src/test/java/io/debezium/connector/oracle/converters/TimestampConverterTests.java
@@ -0,0 +1,431 @@
+package io.debezium.connector.oracle.converters;
+
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import io.debezium.spi.converter.RelationalColumn;
+import io.debezium.spi.converter.CustomConverter.Converter;
+import io.debezium.spi.converter.CustomConverter.ConverterRegistration;
+
+import java.util.OptionalInt;
+import java.util.Properties;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TimestampConverterTests {
+    private class MockRegistration<S> implements ConverterRegistration<S> {
+        public Converter _converter;
+        public S _schema;
+
+        @Override
+        public void register(S fieldSchema, Converter converter) {
+            _converter = converter;
+            _schema = fieldSchema;
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "YY-MM-dd, YY-MM-dd", "," + TimestampConverter2.DEFAULT_DATE_FORMAT })
+    void configureDateTest(final String inputFormat, final String expectedFormat) {
+        final TimestampConverter2 tsConverter = new TimestampConverter2();
+
+        Properties props = new Properties();
+        if (inputFormat != null)
+            props.put("format.date", inputFormat);
+
+        final String beforeConfig = tsConverter.strDateFormat;
+        assertEquals(null, beforeConfig, beforeConfig + " before configuration, should equal " + null);
+        System.out.println(beforeConfig);
+
+        tsConverter.configure(props);
+
+        final String actualResult = tsConverter.strDateFormat;
+        assertEquals(expectedFormat, actualResult,
+                actualResult + " after configuration, should equal " + expectedFormat);
+        System.out.println(actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "mm:ss.SSS, mm:ss.SSS", "," + TimestampConverter2.DEFAULT_TIME_FORMAT })
+    void configureTimeTest(final String inputFormat, final String expectedFormat) {
+        final TimestampConverter2 tsConverter = new TimestampConverter2();
+
+        Properties props = new Properties();
+        if (inputFormat != null)
+            props.put("format.time", inputFormat);
+
+        final String beforeConfig = tsConverter.strTimeFormat;
+        assertEquals(null, beforeConfig, beforeConfig + " before configuration, should equal " + null);
+        System.out.println(beforeConfig);
+
+        tsConverter.configure(props);
+
+        final String actualResult = tsConverter.strTimeFormat;
+        assertEquals(expectedFormat, actualResult,
+                actualResult + " after configuration, should equal " + expectedFormat);
+        System.out.println(actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "date, YYYY-MM-dd, 18368, 2020-04-16", "date,, 18368, 2020-04-16", "time, mm:ss.SSS, 2230, 00:02.230",
+            "time,, 2230, 00:00:02.230", "datetime, YYYY-MM-dd, 1587042000279, 2020-04-16",
+            "datetime,, 1587042000279, 2020-04-16T13:00:00.279Z", "timestamp, YYYY-MM-dd, 1587042000279, 2020-04-16",
+            "datetime2,, 1587042000279, 2020-04-16T13:00:00.279Z", "datetime2, YYYY-MM-dd, 1587042000279, 2020-04-16",
+            "timestamp,, 1587042000279, 2020-04-16T13:00:00.279Z", "date, YYYY-MM-dd, 2019-04-19, 2019-04-19",
+            "datetime,, 2019-04-19 15:13:20.345123, 2019-04-19T15:13:20.345Z", "time,, 15:13:20, 15:13:20.000",
+            "time,HH:mm:ss, 15:13:20, 15:13:20", "timestamp,, 2019-04-19 15:13:20, 2019-04-19T15:13:20.000Z",
+            "datetime,, 19-Apr-2019 15:13:20.345123, 2019-04-19T15:13:20.345Z",
+            "datetime,, 19/04/2019 15:13:20.345123, 2019-04-19T15:13:20.345Z",
+            "datetime,, 2019-4-19 15:13:20.345123, 2019-04-19T15:13:20.345Z",
+            "datetime2,, 2019-4-19 15:13:20.345123, 2019-04-19T15:13:20.345Z",
+            "datetime,, 2019-4-19 3:1:0.345123, 2019-04-19T03:01:00.345Z", "datetime,YYYY-MM-dd,,", "timestamp,,,", "date,,,"})
+    void converterTest(final String columnType, final String format, final String input, final String expectedResult) {
+        final TimestampConverter2 tsConverter = new TimestampConverter2();
+
+        Properties props = new Properties();
+        if (format != null)
+            props.put(
+                    String.format("format.%s",
+                            columnType.equals("timestamp") || columnType.equals("datetime2") ? "datetime" : columnType),
+                    format);
+        props.put("debug", "true");
+        tsConverter.configure(props);
+
+        RelationalColumn mockColumn = getMockColumn(columnType);
+        MockRegistration<SchemaBuilder> mockRegistration = new MockRegistration<SchemaBuilder>();
+
+        tsConverter.converterFor(mockColumn, mockRegistration);
+
+        System.out.println(mockRegistration._schema.name());
+
+        Object actualResult = mockRegistration._converter.convert(input);
+        System.out.println(actualResult);
+        if (actualResult == null) {
+            assertEquals(actualResult, null, String.format(
+                    "columnType: %s, format: %s, input: %s, actualTimeFormat: %s, actualDateFormat: %s, props: %s",
+                    columnType, format, input, tsConverter.strTimeFormat, tsConverter.strDateFormat, props));
+        } else {
+        assertEquals(expectedResult, actualResult,
+                String.format(
+                        "columnType: %s, format: %s, input: %s, actualTimeFormat: %s, actualDateFormat: %s, props: %s",
+                        columnType, format, input, tsConverter.strTimeFormat, tsConverter.strDateFormat, props));
+        }
+    }
+
+    RelationalColumn getMockColumn(String type) {
+        switch (type) {
+            case "date":
+                return getDateColumn();
+            case "time":
+                return getTimeColumn();
+            case "datetime":
+                return getDateTimeColumn();
+            case "datetime2":
+                return getDateTime2Column();
+            case "timestamp":
+                return getTimestampColumn();
+            default:
+                return null;
+        }
+    }
+
+    RelationalColumn getDateColumn() {
+        return new RelationalColumn() {
+
+            @Override
+            public String typeName() {
+                return "date";
+            }
+
+            @Override
+            public String name() {
+                return "datecolumn";
+            }
+
+            @Override
+            public String dataCollection() {
+                return null;
+            }
+
+            @Override
+            public String typeExpression() {
+                return null;
+            }
+
+            @Override
+            public OptionalInt scale() {
+                return null;
+            }
+
+            @Override
+            public int nativeType() {
+                return 0;
+            }
+
+            @Override
+            public OptionalInt length() {
+                return null;
+            }
+
+            @Override
+            public int jdbcType() {
+                return 0;
+            }
+
+            @Override
+            public boolean isOptional() {
+                return false;
+            }
+
+            @Override
+            public boolean hasDefaultValue() {
+                return false;
+            }
+
+            @Override
+            public Object defaultValue() {
+                return null;
+            }
+        };
+    }
+
+    RelationalColumn getTimeColumn() {
+        return new RelationalColumn() {
+
+            @Override
+            public String typeName() {
+                return "time";
+            }
+
+            @Override
+            public String name() {
+                return "timecolumn";
+            }
+
+            @Override
+            public String dataCollection() {
+                return null;
+            }
+
+            @Override
+            public String typeExpression() {
+                return null;
+            }
+
+            @Override
+            public OptionalInt scale() {
+                return null;
+            }
+
+            @Override
+            public int nativeType() {
+                return 0;
+            }
+
+            @Override
+            public OptionalInt length() {
+                return null;
+            }
+
+            @Override
+            public int jdbcType() {
+                return 0;
+            }
+
+            @Override
+            public boolean isOptional() {
+                return false;
+            }
+
+            @Override
+            public boolean hasDefaultValue() {
+                return false;
+            }
+
+            @Override
+            public Object defaultValue() {
+                return null;
+            }
+        };
+    }
+
+    RelationalColumn getDateTimeColumn() {
+        return new RelationalColumn() {
+
+            @Override
+            public String typeName() {
+                return "datetime";
+            }
+
+            @Override
+            public String name() {
+                return "datetimecolumn";
+            }
+
+            @Override
+            public String dataCollection() {
+                return null;
+            }
+
+            @Override
+            public String typeExpression() {
+                return null;
+            }
+
+            @Override
+            public OptionalInt scale() {
+                return null;
+            }
+
+            @Override
+            public int nativeType() {
+                return 0;
+            }
+
+            @Override
+            public OptionalInt length() {
+                return null;
+            }
+
+            @Override
+            public int jdbcType() {
+                return 0;
+            }
+
+            @Override
+            public boolean isOptional() {
+                return false;
+            }
+
+            @Override
+            public boolean hasDefaultValue() {
+                return true;
+            }
+
+            @Override
+            public Object defaultValue() {
+                return null;
+            }
+        };
+    }
+
+    RelationalColumn getDateTime2Column() {
+        return new RelationalColumn() {
+
+            @Override
+            public String typeName() {
+                return "datetime2";
+            }
+
+            @Override
+            public String name() {
+                return "datetime2column";
+            }
+
+            @Override
+            public String dataCollection() {
+                return null;
+            }
+
+            @Override
+            public String typeExpression() {
+                return null;
+            }
+
+            @Override
+            public OptionalInt scale() {
+                return null;
+            }
+
+            @Override
+            public int nativeType() {
+                return 0;
+            }
+
+            @Override
+            public OptionalInt length() {
+                return null;
+            }
+
+            @Override
+            public int jdbcType() {
+                return 0;
+            }
+
+            @Override
+            public boolean isOptional() {
+                return false;
+            }
+
+            @Override
+            public boolean hasDefaultValue() {
+                return false;
+            }
+
+            @Override
+            public Object defaultValue() {
+                return null;
+            }
+        };
+    }
+
+    RelationalColumn getTimestampColumn() {
+        return new RelationalColumn() {
+
+            @Override
+            public String typeName() {
+                return "timestamp";
+            }
+
+            @Override
+            public String name() {
+                return "timestampcolumn";
+            }
+
+            @Override
+            public String dataCollection() {
+                return null;
+            }
+
+            @Override
+            public String typeExpression() {
+                return null;
+            }
+
+            @Override
+            public OptionalInt scale() {
+                return null;
+            }
+
+            @Override
+            public int nativeType() {
+                return 0;
+            }
+
+            @Override
+            public OptionalInt length() {
+                return null;
+            }
+
+            @Override
+            public int jdbcType() {
+                return 0;
+            }
+
+            @Override
+            public boolean isOptional() {
+                return false;
+            }
+
+            @Override
+            public boolean hasDefaultValue() {
+                return false;
+            }
+
+            @Override
+            public Object defaultValue() {
+                return null;
+            }
+        };
+    }
+}
